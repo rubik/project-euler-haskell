ff1c7af8f9565b403910931585b7c99579813b20
Michele Lacchia <michelelacchia@gmail.com>
2014-07-11 12:32:45 +0200

    Add utils module

--- 3.hs
+++ 3.hs
@@ -1,22 +1,7 @@
 module Main
     where
 
-divides :: Integer -> Integer -> Bool
-divides d n = rem n d == 0
-
-ldf :: Integer -> Integer -> Integer
-ldf k n
-    | k `divides` n = k
-    | k^2 > n       = n
-    | otherwise     = ldf (k+1) n
-
-ld :: Integer -> Integer
-ld = ldf 2
-
-factors :: Integer -> [Integer]
-factors 1 = []
-factors n = let p = ld n
-                  in p : factors (n `div` p)
+import Utils (factors)
 
 main :: IO ()
 main = print . maximum . factors $ 600851475143
---
+++ Utils.hs
@@ -0,0 +1,29 @@
+module Utils
+    where
+
+import Data.List
+
+encode :: (Eq a) => [a] -> [(a, Int)]
+encode l = [(head i, length i) | i <- group l]
+
+lf :: Integer -> Integer
+lf 1 = error "Numbers lesser than 2 not allowed"
+lf n
+    | n `mod` 2 == 0 = 2
+    | otherwise      = head . filter (\i -> n `mod` i == 0) $ [3,5..]
+
+factors :: Integer -> [Integer]
+factors 1 = []
+factors n = let l = lf n
+             in l : factors (n `div` l)
+
+factors' :: Integer -> [(Integer, Int)]
+factors' = encode . factors
+
+ndivisors :: Integer -> Int
+ndivisors = product . map ((+1) . snd) . factors'
+
+divisors :: Integer -> [Integer]
+divisors n = concatMap (\x -> [x, n `div` x]) .
+             filter (\x -> n `mod` x == 0) .
+             takeWhile (\x -> x*x <= n) $ [1..]
