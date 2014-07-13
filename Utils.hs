bbdf8478d5dc308e2032f8c38f167818cd03a03a
Michele Lacchia <michelelacchia@gmail.com>
2014-07-13 22:53:24 +0200

    Improve Utils

--- Utils.hs
+++ Utils.hs
@@ -10,7 +10,10 @@ lf :: Integer -> Integer
 lf 1 = error "Numbers < 2 not allowed"
 lf n
     | n `mod` 2 == 0 = 2
-    | otherwise      = head . filter (\i -> n `mod` i == 0) $ [3,5..]
+    | otherwise      = if null a then n else head a
+        where
+            l = floor . sqrt . fromIntegral $ n
+            a = filter (\i -> n `mod` i == 0) [3,5..l]
 
 factors :: Integer -> [Integer]
 factors 1 = []
@@ -28,6 +31,21 @@ divisors n = concatMap (\x -> [x, n `div` x]) .
              filter (\x -> n `mod` x == 0) .
              takeWhile (\x -> x*x <= n) $ [1..]
 
-factorial :: Int -> Int
+primes :: [Integer]
+primes = 2 : filter ((==1) . length . factors) [3,5..]
+
+factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial (n-1)
+
+totient :: Integer -> Integer
+totient n = (m*) . product . map ((\p -> p-1) . fst) $ f
+    where
+        f = factors' n
+        m = product . map (\(p,e) -> p^(e-1)) $ f
+
+-- Using Arrows
+{-totient n = uncurryN div . (num &&& product) $ f
+    where
+        f = map fst . factors' $ n
+        num = (n*) . product . map (\p -> p-1)-}
