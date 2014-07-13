c333f406057ba785f524065a1e52cdc36e2ced7b
Michele Lacchia <michelelacchia@gmail.com>
2014-07-14 00:18:27 +0200

    Add solution to problem 21

---
+++ 021.hs
@@ -0,0 +1,11 @@
+module Main
+    where
+
+import Data.List
+import Utils
+
+main :: IO ()
+main = print . sum . map fst . filter (\(i,n) -> n < 10000 && i /= n &&
+        i == k !! (fromInteger $ n - 1)) . zip [1..] $ k
+    where
+        k = map (sum . init . sort . divisors) [1..10000]
