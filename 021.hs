721bb31a9a476bd4081e935fe8426be501cd3e7f
Michele Lacchia <michelelacchia@gmail.com>
2014-07-14 08:25:10 +0200

    Remove redundant parens

--- 021.hs
+++ 021.hs
@@ -6,6 +6,6 @@ import Utils
 
 main :: IO ()
 main = print . sum . map fst . filter (\(i,n) -> n < 10000 && i /= n &&
-        i == k !! (fromInteger $ n - 1)) . zip [1..] $ k
+        i == k !! fromInteger (n - 1)) . zip [1..] $ k
     where
         k = map (sum . init . sort . divisors) [1..10000]
